9626da1ff74b8c899ea8b0fe9fdee2c59157b169
ahammani <anas.hammani@mirakl.com>
2024-05-20 10:50:14 +0200

    Make screenshot web compatible

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState, useRef } from 'react';
-import { StyleSheet, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {useState, useRef} from 'react';
+import {StyleSheet, View, Platform} from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as MediaLibrary from 'expo-media-library';
-import { captureRef } from 'react-native-view-shot';
+import {captureRef} from 'react-native-view-shot';
+
+import domtoimage from 'dom-to-image';
 
 
 import ImageViewer from "./components/ImageViewer";
@@ -17,18 +19,18 @@ import {GestureHandlerRootView} from "react-native-gesture-handler";
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 export default function App() {
-    const [selectedImage, setSelectedImage] = useState(null);
+    const [selectedImage, setSelectedImage]   = useState(null);
     const [showAppOptions, setShowAppOptions] = useState(false);
     const [isModalVisible, setIsModalVisible] = useState(false);
-    const [pickedEmoji, setPickedEmoji] = useState(null);
-    const [status, requestPermission] = MediaLibrary.usePermissions();
-    const imageRef = useRef();
+    const [pickedEmoji, setPickedEmoji]       = useState(null);
+    const [status, requestPermission]         = MediaLibrary.usePermissions();
+    const imageRef                            = useRef();
 
-    if(status == null) {
+    if (status == null) {
         requestPermission();
     }
     const pickImageAsync = async () => {
-       let result= await ImagePicker.launchImageLibraryAsync({
+        let result = await ImagePicker.launchImageLibraryAsync({
             allowsEditing: true,
             quality: 1
         });
@@ -53,65 +55,83 @@ export default function App() {
     };
 
     const onSaveImageAsync = async () => {
-        try {
-            const localUri = await captureRef(imageRef, {
-                height: 440,
-                quality: 1,
-            });
+        if (Platform.OS !== 'web') {
+            try {
+                const localUri = await captureRef(imageRef, {
+                    height: 440,
+                    quality: 1,
+                });
 
-            await MediaLibrary.saveToLibraryAsync(localUri);
-            if (localUri) {
-                alert("Saved!");
+                await MediaLibrary.saveToLibraryAsync(localUri);
+                if (localUri) {
+                    alert("Saved!");
+                }
+            } catch (e) {
+                console.log(e);
             }
-        } catch (e) {
-            console.log(e);
+        } else {
+            try {
+                const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+                    quality: 0.95,
+                    width: 320,
+                    height: 440,
+                });
+
+                let link = document.createElement('a');
+                link.download = 'sticker-smash.jpeg';
+                link.href = dataUrl;
+                link.click();
+                alert('saved !')
+            } catch (e) {
+                console.log(e);
+            }
         }
     };
 
-  return (
-    <GestureHandlerRootView style={styles.container}>
-      <View style={styles.imageContainer}>
-          <View ref={imageRef} collapsable={false}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
-          {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
-          </View>
-      </View>
-        {showAppOptions?
-            <View style={styles.optionsContainer} >
-                <View style={styles.optionsRow}>
-                    <IconButton icon="refresh" label="Reset" onPress={onReset} />
-                    <CircleButton onPress={onAddSticker} />
-                    <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+    return (
+        <GestureHandlerRootView style={styles.container}>
+            <View style={styles.imageContainer}>
+                <View ref={imageRef} collapsable={false}>
+                    <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}/>
+                    {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji}/>}
                 </View>
             </View>
-            : (
-          <View style={styles.footerContainer}>
-            <Button label="Choose a photo" theme="primary" onPress={pickImageAsync}/>
-            <Button label="Use this photo" onPress={()=> setShowAppOptions(true)} />
-          </View>
-        )}
-        <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-            <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
-        </EmojiPicker>
-      <StatusBar style="auto" />
-    </GestureHandlerRootView>
-  );
+            {showAppOptions ?
+                <View style={styles.optionsContainer}>
+                    <View style={styles.optionsRow}>
+                        <IconButton icon="refresh" label="Reset" onPress={onReset}/>
+                        <CircleButton onPress={onAddSticker}/>
+                        <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync}/>
+                    </View>
+                </View>
+                : (
+                    <View style={styles.footerContainer}>
+                        <Button label="Choose a photo" theme="primary" onPress={pickImageAsync}/>
+                        <Button label="Use this photo" onPress={() => setShowAppOptions(true)}/>
+                    </View>
+                )}
+            <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+                <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose}/>
+            </EmojiPicker>
+            <StatusBar style="auto"/>
+        </GestureHandlerRootView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-  },
-  imageContainer: {
-    flex: 1,
-    paddingTop: 58,
-  },
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+    },
+    imageContainer: {
+        flex: 1,
+        paddingTop: 58,
+    },
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
+    },
     optionsContainer: {
         position: 'absolute',
         bottom: 80,
